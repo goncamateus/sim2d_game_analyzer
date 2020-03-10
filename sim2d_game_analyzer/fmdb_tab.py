ea7b1755f68a1d4f25dde2dd7b0cd1be377f10a0
goncamateus <mgm4@cin.ufpe.br>
2020-03-09 22:47:16 -0300

    draws pre-saved points

--- sim2d_game_analyzer/fmdb_tab.py
+++ sim2d_game_analyzer/fmdb_tab.py
@@ -1,9 +1,11 @@
+import pandas as pd
 from PyQt5.QtCore import QEvent, QPoint, Qt
 from PyQt5.QtGui import QBrush, QIcon, QPainter, QPen
 from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox, QDialog,
-                             QDialogButtonBox, QGroupBox, QHBoxLayout, QLabel,
-                             QLineEdit, QSlider, QStyleOptionSlider, QToolTip,
-                             QVBoxLayout, QWidget)
+                             QDialogButtonBox, QFileDialog, QGroupBox,
+                             QHBoxLayout, QLabel, QLineEdit, QSlider,
+                             QStyleOptionSlider, QToolTip, QVBoxLayout,
+                             QWidget)
 
 PITCH_LENGHT = 105.0
 PITCH_WIDTH = 68.0
@@ -15,6 +17,7 @@ CORNER_ARC_R = 1.0
 PENALTY_CIRCLE_R = 9.15
 PENALTY_SPOT_DIST = 11.0
 PLAYER_SIZE = 0.3
+BALL_SIZE = 0.085
 
 
 class TipSlider(QSlider):
@@ -26,8 +29,6 @@ class TipSlider(QSlider):
         self.opt = QStyleOptionSlider()
 
         self.valueChanged.connect(self.show_tip)
-        # self.enterEvent = self.show_tip
-        # self.mouseReleaseEvent = self.show_tip
 
     def show_tip(self, _):
         self.initStyleOption(self.opt)
@@ -40,8 +41,16 @@ class TipSlider(QSlider):
 
 
 class Field(QGroupBox):
-    def __init__(self):
+    def __init__(self, parent):
         super(Field, self).__init__("")
+        self.base_data = None
+        self.dbs = [None for _ in range(11)]
+        self.selected_player = 1
+        self.selected_class = "Good"
+        self.saved_points = {key: [list() for _ in range(11)]
+                             for key in range(1, 6001)}
+        self.time = 1
+        self.save_columns = []
         self.mx = 1
         self.my = 1
         self.fmid = 0
@@ -49,6 +58,33 @@ class Field(QGroupBox):
         self.players_l = [None for _ in range(11)]
         self.players_r = [None for _ in range(11)]
         self.setMouseTracking(True)
+
+    def reset(self):
+        self.dbs = [None for _ in range(11)]
+        self.saved_points = {key: [list() for _ in range(11)]
+                             for key in range(1, 6001)}
+
+    def set_time(self, time):
+        self.time = time
+        self.update()
+
+    def set_base(self, df):
+        self.base_data = df
+        columns = ['show_time', 'ball_x', 'ball_y']
+        player_l = ['player_lNUMBER_x'.replace(
+            'NUMBER', str(num + 1)) for num in range(11)]
+        player_l = player_l + ['player_lNUMBER_y'.replace(
+            'NUMBER', str(num + 1)) for num in range(11)]
+        player_l.sort(key=lambda x: int(x.split('_')[1][1:]))
+        player_r = ['player_rNUMBER_x'.replace(
+            'NUMBER', str(num + 1)) for num in range(11)]
+        player_r = player_r + ['player_rNUMBER_y'.replace(
+            'NUMBER', str(num + 1)) for num in range(11)]
+        player_r.sort(key=lambda x: int(x.split('_')[1][1:]))
+        columns = columns + player_l + player_r
+        self.base_data = self.base_data[columns]
+        self.time = 1
+        self.update()
 
     def mouseMoveEvent(self, e):
         mid_x, mid_y = self.fmid
@@ -58,14 +94,88 @@ class Field(QGroupBox):
         self.setToolTip(text)
         QToolTip.showText(QPoint(e.x(), e.y()), str(text), self)
 
+    def mousePressEvent(self, e):
+        if self.base_data is not None:
+            mid_x, mid_y = self.fmid
+            self.cx = (e.x() - mid_x)/12
+            self.cy = (e.y() - mid_y)/12
+            db = self.dbs[self.selected_player - 1]
+            if db is None:
+                db = list()
+            at_time = self.base_data[self.base_data['show_time']
+                                     == self.time].iloc[0]
+            my_array = list()
+            for key in at_time.keys():
+                if not key in ['show_time', f'player_l{self.selected_player}_x',
+                               f'player_l{self.selected_player}_y']:
+                    my_array.append(at_time[key])
+                elif key != 'show_time':
+                    if key.endswith('x'):
+                        my_array.append(self.cx)
+                    else:
+                        my_array.append(self.cy)
+            if self.selected_class == "Good":
+                my_array.append(1)
+            else:
+                my_array.append(0)
+            db.append(my_array)
+            self.save_columns = [x for x in at_time.keys() if x != 'show_time']
+            self.save_columns.append('class')
+            self.dbs[self.selected_player - 1] = db
+            self.saved_points[self.time][self.selected_player -
+                                         1].append((self.cx, self.cy))
+            self.update()
+
     def paintEvent(self, e):
         painter = QPainter(self)
         field_brush = QBrush(Qt.green, Qt.SolidPattern)
         painter.setRenderHint(QPainter.HighQualityAntialiasing, True)
-        painter.setPen(QPen(Qt.white, 1, Qt.SolidLine))
+        painter.setPen(QPen(Qt.white, 3, Qt.SolidLine))
         painter.setBrush(field_brush)
         self.draw_background(painter, field_brush)
         self.draw_lines(painter)
+        if self.base_data is not None:
+            self.draw_players(painter)
+            self.draw_ball(painter)
+        if len(self.saved_points[self.time][self.selected_player - 1]) > 0:
+            self.draw_saved_points(painter)
+
+    def draw_saved_points(self, painter):
+        painter.setPen(QPen(Qt.white, 5, Qt.SolidLine))
+        painter.setBrush(QBrush(Qt.green, Qt.SolidPattern))
+        for point_x, point_y in self.saved_points[self.time][
+                self.selected_player - 1]:
+            painter.drawPoint(
+                point_x*12 + self.fmid[0], point_y*12 + self.fmid[1])
+
+    def draw_players(self, painter):
+        at_time = self.base_data[self.base_data['show_time']
+                                 == self.time].iloc[0]
+        # Left players
+        for side, color in [('l', Qt.blue), ('r', Qt.red)]:
+            for i in range(1, 12):
+                if i == self.selected_player and side == 'l':
+                    painter.setPen(QPen(Qt.yellow, 10, Qt.SolidLine))
+                else:
+                    painter.setPen(QPen(color, 10, Qt.SolidLine))
+                x = at_time[f'player_{side}{i}_x']*12
+                y = at_time[f'player_{side}{i}_y']*12
+                x = self.fmid[0] + x
+                y = self.fmid[1] + y
+                painter.drawEllipse(
+                    QPoint(x, y),
+                    PLAYER_SIZE*12, PLAYER_SIZE*12)
+
+    def draw_ball(self, painter):
+        at_time = self.base_data[self.base_data['show_time']
+                                 == self.time].iloc[0]
+        x = self.fmid[0] + at_time.ball_x*12
+        y = self.fmid[1] + at_time.ball_y*12
+        painter.setPen(QPen(Qt.white, 8, Qt.SolidLine))
+        painter.setBrush(QBrush(Qt.white, Qt.SolidPattern))
+        painter.drawEllipse(
+            QPoint(x, y),
+            PLAYER_SIZE*10, PLAYER_SIZE*10)
 
     def draw_background(self, painter, field_brush):
         painter.fillRect(painter.window(), field_brush)
@@ -110,6 +220,7 @@ class FMDBTab(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.db = pd.DataFrame()
         self.mainLayout = QVBoxLayout()
         self.optionsGroup = QGroupBox("")
         self.optionsLayout = QHBoxLayout()
@@ -118,23 +229,52 @@ class FMDBTab(QWidget):
         self.create_players_GB()
         self.create_slider()
         self.mainLayout.addWidget(self.optionsGroup)
-        self.field = Field()
+        self.field = Field(self)
         self.mainLayout.addWidget(self.field)
         self.optionsGroup.setMaximumHeight(70)
         buttonbox = QDialogButtonBox(
-            QDialogButtonBox.Save | QDialogButtonBox.Cancel)
-        buttonbox.accepted.connect(self.save)
-        buttonbox.rejected.connect(self.cancel)
+            QDialogButtonBox.Open |
+            QDialogButtonBox.Save |
+            QDialogButtonBox.Cancel)
+        buttonbox.buttons()[1].clicked.connect(self.open)
+        buttonbox.buttons()[0].clicked.connect(self.save)
+        buttonbox.buttons()[2].clicked.connect(self.cancel)
         self.mainLayout.addWidget(buttonbox)
         self.setLayout(self.mainLayout)
         self.setFocusPolicy(Qt.StrongFocus)
         self.setFocus(True)
 
+    def open(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(
+            self, "Open File", "",
+            "All Files (*);;CSV Files (*.csv)", options=options)
+        if fileName:
+            base_df = pd.read_csv(fileName)
+            self.field.set_base(base_df)
+
     def save(self):
-        pass
+        self.dbs = self.field.dbs
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getSaveFileName(
+            self, "Save Dataset", "",
+            "All Files (*);;CSV Files (*.csv)", options=options)
+        if fileName:
+            fileName = fileName.replace('csv', '')
+            for unum, db in enumerate(self.dbs):
+                name = fileName + str(unum + 1) + '.csv'
+                df = pd.DataFrame(
+                    db, columns=self.field.save_columns)
+                df.to_csv(name, index=False)
 
     def cancel(self):
-        pass
+        self.dbs = [None for _ in range(11)]
+        self.combo_classes.setCurrentText('Good')
+        self.combo_players.setCurrentText('1')
+        self.sld.setValue(1)
+        self.field.reset()
 
     def create_class_GB(self):
         my_list = ["Good", "Bad"]
@@ -158,18 +298,39 @@ class FMDBTab(QWidget):
         self.sld.setGeometry(325, 20, PITCH_LENGHT*9, 20)
         self.sld.setTickInterval(100)
         self.sld.setTickPosition(QSlider.TicksBelow)
+        self.sld.valueChanged.connect(
+            lambda: self.field.set_time(self.sld.value()))
         self.optionsLayout.addWidget(self.sld)
 
     def change_class(self, value):
         self.SELECTED_CLASS = value
+        self.field.selected_class = value
 
     def change_player(self, value):
         self.SELECTED_PLAYER = value
+        self.field.selected_player = int(value)
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Right:
             self.sld.setValue(self.sld.value() + 1)
         elif event.key() == Qt.Key_Left:
             self.sld.setValue(self.sld.value() - 1)
+        elif event.key() == Qt.Key_G:
+            self.combo_classes.setCurrentText('Good')
+        elif event.key() == Qt.Key_B:
+            self.combo_classes.setCurrentText('Bad')
+        elif event.key() == 45:
+            self.combo_players.setCurrentText('11')
+        elif event.key() > 47 and event.key() < 58:
+            num = int(event.key()) - 48
+            num = num if num != 0 else 10
+            self.combo_players.setCurrentText(f'{num}')
+        elif event.key() == Qt.Key_Escape:
+            self.cancel()
+        elif event.key() == Qt.Key_O:
+            self.open()
+        elif event.key() == Qt.Key_S:
+            self.save()
         else:
             QWidget.keyPressEvent(self, event)
+        self.field.set_time(self.sld.value())
